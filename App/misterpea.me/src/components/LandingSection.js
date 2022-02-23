05f8dfa93aacf473605986dd1f6a54a4303aa54e
Perry Angelora <peamaillist@gmail.com>
2022-02-22 22:38:17 -0500

    Initial commit

---
+++ App/misterpea.me/src/components/LandingSection.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export default function LandingSection() {
+  return (
+    <div className='section landing-sec'>
+      <div className='lines top'></div>
+      <div className='logo-large'>
+        <h1>Perry</h1>
+        <h1>Angelora</h1>
+      </div>
+      <div className='tagline'>
+        <div className='tagline-wrapper'>
+          <p>Developer; polymath; autodidact;</p>
+          <p> user of ostentatious words.</p>
+        </div>
+      </div>
+      <div className='lines bottom'></div>
+    </div>
+  );
+}
