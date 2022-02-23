eb36efe15e2b4b3ad6e92fe7bf3db7e7d55962ca
Perry Angelora <peamaillist@gmail.com>
2022-02-22 22:39:02 -0500

    Added <p> tags within <li>

--- App/misterpea.me/src/components/Navbar.js
+++ App/misterpea.me/src/components/Navbar.js
@@ -11,9 +11,15 @@ export default function Navbar() {
       <div className='site-title'>mister<span>pea</span>.me</div>
       <nav>
         <ul className='nav-ul'>
-          <li role="button" tabIndex={0}>about</li>
-          <li role="button" tabIndex={0}>wares</li>
-          <li role="button" tabIndex={0}>connect</li>
+          <li role="button" tabIndex={0}>
+            <p>about</p>
+          </li>
+          <li role="button" tabIndex={0}>
+            <p>wares</p>
+          </li>
+          <li role="button" tabIndex={0}>
+            <p>connect</p>
+          </li>
           <li role="button" tabIndex={0} onClick={toggleTheme}>{
             currentTheme === "light" ?
               <BsFillMoonFill className='theme-icon' /> :
