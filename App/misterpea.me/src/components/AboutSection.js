dc1e249d7ff117c6f3f3961e07444cade21b7cc5
Perry Angelora <peamaillist@gmail.com>
2022-03-11 12:49:53 -0500

    Added image/canvas component

--- App/misterpea.me/src/components/AboutSection.js
+++ App/misterpea.me/src/components/AboutSection.js
@@ -1,16 +1,17 @@
 import * as React from 'react';
+import AboutImageCanvas from './Canvas';
 
 import { useState, useEffect, useRef } from 'react';
 
-export default function AboutSection() {
+export default function AboutSection({ currentTheme }) {
   const [moreText, setMoreText] = useState(false);
   const [hover, setHover] = useState(false);
   const moreTextHeight = useRef(0);
 
   useEffect(() => {
-    moreTextHeight.current = `${document.querySelector('.more-text').scrollHeight}px`
+    moreTextHeight.current = `${document.querySelector('.more-text').scrollHeight}px`;
   });
-  
+
   function handleTextToggle() {
     setMoreText((s) => !s);
   }
@@ -33,7 +34,10 @@ export default function AboutSection() {
       <div className='main-content'>
         <h1 className='section-title'>about:</h1>
         <div className='section-text-wrapper'>
-        <div className='image'></div>
+          {currentTheme === 'dark' ?
+            <div key="dark" className='image dark'><AboutImageCanvas darkMode={true} /></div> :
+            <div key="light" className='image light' ><AboutImageCanvas darkMode={false} /></div>
+          }
           <div className='section-text'>
             <p>Mister Pea is me, Perry Angelora, and this website is the home-base
               of my programmatical wares. I am a serial-creator, a player of pianos,
@@ -41,7 +45,7 @@ export default function AboutSection() {
             </p>
             <p
               className={`more-text ${moreText ? "visible" : "hidden"}`}
-              style={{height: `${moreText ? moreTextHeight.current : "0px"}`}}
+              style={{ height: `${moreText ? moreTextHeight.current : "0px"}` }}
             >I earned my BFA from the University of Central Florida and my
               MFA from the Pratt Institute, Brooklyn. I&rsquo;ve been the recipient of the New York
               Foundation for the Arts Fellowship in Printmaking/Drawing/Book Arts and a finalist for
@@ -57,7 +61,6 @@ export default function AboutSection() {
               {moreText ? <p className={moreText ? "less" : ""}><span>←</span>less</p> : <p className={moreText ? "" : "more"}>more<span>→</span></p>}
             </div>
           </div>
-         
         </div>
       </div>
       <div className='lines bottom' />
