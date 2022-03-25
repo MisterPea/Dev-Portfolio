8e8884e4f217bf60698c9db7190c163258c0b0f5
Perry Angelora <peamaillist@gmail.com>
2022-03-25 19:06:44 -0400

    Text changes; Lighthouse impovements

--- App/misterpea.me/pages/_app.js
+++ App/misterpea.me/pages/_app.js
@@ -5,6 +5,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Mister Pea - Developer; Aspiring Raconteur</title>
+        <meta property='og:title' content='Mister Pea - Developer, Aspiring Raconteur' key="title" />
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, viewport-fit=cover" />
       </Head>
       <Component {...pageProps} />
